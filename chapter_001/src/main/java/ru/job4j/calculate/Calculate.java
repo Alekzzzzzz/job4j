c43eb3cd534ad67e2a20d2879166e3d668bb0dc3
Petr Arsentev <Proton1987@yandex.ru>
2019-07-08 16:06:52 +0300

    Изменили Calculate [#157494]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,13 @@
 package ru.job4j.calculate;
-
+/**
+*Calculate.
+*
+*@author Alexander Gaponenko (Proton1987@yandex.ru
+*/
 public class Calculate {   
-
+/**
+*Main
+*/
    public static void main(String[] args) {
       System.out.println("Hello, Job4j.");
    }
