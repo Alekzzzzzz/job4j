5dfe0c284e142854ce4f682e8508ee718e68009b
Petr Arsentev <Proton1987@yandex.ru>
2019-07-12 12:07:41 +0300

    "Calculator [#157485]"

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -4,7 +4,19 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test
+ *
+ * @author Alexander Gaponenko (Proton1987@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class CalculatorTest  {
+    /**
+     * Test add
+     */
+
     @Test
     public void whenAddOnePlusOneThenTwo()  {
         Calculator calc = new Calculator();
@@ -12,24 +24,39 @@ public class CalculatorTest  {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Test subtract
+     */
+
     @Test
     public void whenAddOneMinusOneThenTwo()  {
         Calculator calc = new Calculator();
-        double result = calc.subtract(1D, 1D);
+        double result = calc.subtract(4D, 2D);
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Test div
+     */
+
     @Test
     public void whenAddOneDivOneThenTwo()  {
         Calculator calc = new Calculator();
-        double result = calc.div(1D, 1D);
+        double result = calc.div(4D, 2D);
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Test multiple
+     */
+
     @Test
     public void whenAddOneMultipleOneThenTwo()  {
         Calculator calc = new Calculator();
-        double result = calc.multiple(1D, 1D);
+        double result = calc.multiple(2D, 1D);
         double expected = 2D;
         assertThat(result, is(expected));
     }
