4c5cf5388ce306d4c56a5c96f192b5c57d5d304c
Petr Arsentev <Proton1987@yandex.ru>
2019-08-04 17:22:30 +0300

    "5.3. Построить шахматную доску в псевдографике.[#157461]"

--- chapter_001/src/main/java/Main.java
+++ chapter_001/src/main/java/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        String s = String.format("Privet %s %s","Alexander","fsf");
+        System.out.println(s.toLowerCase());
 
     }
 }
