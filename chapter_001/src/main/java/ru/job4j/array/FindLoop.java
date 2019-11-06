14c884d3f7e6d00baad4c3872eb7de2a056dc89e
Petr Arsentev <Proton1987@yandex.ru>
2019-11-06 20:14:48 +0300

    6.5.1. Сортировка выборкой.[#157478]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,7 +1,7 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el, int start, int finish) {
+    public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = start; index < finish; index++) {// проходим по циклу от 0 до выбранного числа
             if (data[index] == el) {// если индекс = выбранному числу
--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -1,11 +1,23 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        FindLoop FindLoop = new FindLoop();
-    //    int min = MinDiapason.findMin(data, 0, data.length);
-    //    int index = FindLoop.indexOf(data, min, 0, data.length);
+        for (int i = 0, temp; i < data.length; i++) {
+
+            int min = MinDiapason.findMin(data, i, data.length);
+
+            int index = (int) FindLoop.indexOf(data, min, i, data.length);
+
+            // Swap the values
+            temp = data[i];
+            data[i] = data[index];
+            data[index] = temp;
+
+            System.out.println(Arrays.toString(data));
+        }
         return data;
     }
 }
