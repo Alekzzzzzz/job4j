ecec0d3662c3a308d100697170f4e5512d25c395
Alekzzzzzz <proton1987@yandex.ru>
2020-04-27 14:00:40 +0300

    noutbuk

--- chapter_001/src/main/java/ru/job4j/loop/Main.java
+++ chapter_001/src/main/java/ru/job4j/loop/Main.java
@@ -1,7 +1,19 @@
 package ru.job4j.loop;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int massiv [][]= new int [3][3];
+        int count = 0;
+        for (int i = 0; i != 3; i++){
+        for (int j = 0; j != 3; j++){
+            massiv[i][j]= count++;
+        }
+
+        }   for (int index = 0; index != 3 ; index++){
+            System.out.println(Arrays.toString(massiv[index]));
+        }
 
     }
 }
