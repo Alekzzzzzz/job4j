4f61ce674f47f480dd173c3fd0d4742dfa56899f
Alekzzzzzz <proton1987@yandex.ru>
2020-05-03 21:10:03 +0300

    новая ветка и репозиторий

--- chapter_001/src/main/java/ru/job4j/loop/Main.java
+++ chapter_001/src/main/java/ru/job4j/loop/Main.java
@@ -4,16 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int massiv [][]= new int [3][3];
-        int count = 0;
-        for (int i = 0; i != 3; i++){
-        for (int j = 0; j != 3; j++){
-            massiv[i][j]= count++;
+        int [] a = new int [10];
+        for (int i = 0;i < a.length-1; i++){
+            a[i] = i + 1 ;
+            System.out.println(a[i]);
         }
-
-        }   for (int index = 0; index != 3 ; index++){
-            System.out.println(Arrays.toString(massiv[index]));
-        }
-
     }
 }
