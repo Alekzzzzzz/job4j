2349b708ab31b6078f8a2ee53d77ca6b62dfcdf8
Petr Arsentev <Proton1987@yandex.ru>
2020-05-15 23:19:14 +0300

    "Реализовать класс Tracker [#157572]"

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -28,45 +28,43 @@ public class Tracker {
         Item[] namesWithoutNull = new Item[items.length];
         int size = 0;
         for (int index = 0; index < items.length; index++) {
-            Item name = items[index];
-            if (name != null) {
-                namesWithoutNull[size] = name;
+            Item item = items[index];
+            if (item != null) {
+                namesWithoutNull[size] = item;
                 size++;
             }
-            namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+
         }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
         return namesWithoutNull;
     }
 
     public Item[] findByName(String key){
-        Item item = new Item(key);
+        int size = 0;
         Item [] newArray = new Item[items.length];
-        for (int i = 0; i < items.length;i++) {
-            Item name = items[i];
-            if (item.getName().equals(key)) {
-                newArray[this.position] = name;
-                this.position++;
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null){
+                if (key.equals(item.getName())){
+                    newArray[size] = item;
+                    size++;
+                }
             }
         }
-        newArray = Arrays.copyOf(newArray,position);
+        newArray = Arrays.copyOf(newArray,size);
         return newArray;
     }
 
-    public Item findById(String id){
-        Item item = new Item(id);
-        Item [] newArray = new Item[items.length];
+    public Item  findById(String id){
         for (int i = 0; i < items.length; i++){
-            Item name = items[i];
-            if (item.getId().equals(id)){
-                newArray[position] = items[i];
-                position++;
-            }else {
-                newArray[position] = null;
-                position++;
+            Item currentItem = items[i];
+            if (currentItem != null){
+                if (currentItem.getId().equals(id)){
+                    return currentItem;
+                }
             }
-
         }
-        return newArray[position];
+        return null;
     }
 
     /**
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -3,9 +3,10 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class TrackerTest {
     @Test
@@ -17,5 +18,50 @@ public class TrackerTest {
         assertThat(result.getName(), is(item.getName()));
     }
 
+    @Test
+    public void shouldReturnItemsByName() {
+        Tracker tracker = new Tracker();
+        String name = "test2";
 
+        Item item1 = new Item(name);
+        Item item2 = new Item(name);
+        Item item3 = new Item(name);
+
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+
+        Item[] result = tracker.findByName(name);
+        assertEquals(3, result.length);
+        assertTrue(name.equals(result[0].getName()) &&
+                name.equals(result[1].getName()) &&
+                name.equals(result[2].getName()));
+
+    }
+
+
+    @Test
+    public void shouldReturnFindAll() {
+        Tracker tracker = new Tracker();
+        String name1 = "name1";
+        String name2 = "name2";
+        String name3 = "name3";
+
+        Item item1 = new Item(name1);
+        Item item2 = new Item(name2);
+        Item item3 = new Item(name3);
+
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+
+        Item [] result = tracker.findAll();
+        assertEquals(3,result.length);
+        assertTrue( name1.equals(result[0].getName()) &&
+                              name2.equals(result[1].getName()) &&
+                              name3.equals(result[2].getName()));
+        assertEquals(item1, result[0]);
+        assertEquals(item2, result[1]);
+        assertEquals(item3, result[2]);
+    }
 }
